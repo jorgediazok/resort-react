222555475a786d120e95e8cca6d497a52af1afaf
Jorge <jorgediazok@gmail.com>
2020-02-07 18:13:29 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Rooms from './pages/Rooms';
+import Error from './pages/Error';
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+      <Home />
+      <SingleRoom />
+      <Rooms />
+      <Error />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Error() {
+  return <div>Hello from Error Page</div>;
+}
+
+export default Error;
