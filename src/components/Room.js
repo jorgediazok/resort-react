a1e6209627f190247cc2d6475ba321cd0456ce37
Jorge <jorgediazok@gmail.com>
2020-02-09 13:30:55 -0300

    no encuentro el errorgit add .

---
+++ src/components/Room.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Room() {
+  return <div>Hello from Room</div>;
+}
+
+export default Room;
--- src/context.js
+++ src/context.js
@@ -13,8 +13,15 @@ class RoomProvider extends Component {
 
   //getData
   componentDidMount() {
+    //Data
     let rooms = this.formatData(items);
-    console.log(rooms);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    });
   }
 
   formatData(items) {
